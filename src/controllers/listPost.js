976128820a7bc07cbd20dea2fd3e1c31d09a3df4
omnya zerghouni <omnyazer@gmail.com>
2026-04-16 21:07:08 +0200

    ajout style

--- src/controllers/listPost.js
+++ src/controllers/listPost.js
@@ -2,14 +2,8 @@ const BlogPost = require('../models/blogPost');
 
 module.exports = (req, res) => {
   const search = req.query.search ? req.query.search.trim() : '';
-  const query = search
-    ? {
-      userid: { $exists: true, $ne: null },
-      title: { $regex: search, $options: 'i' }
-    }
-    : { userid: { $exists: true, $ne: null } };
 
-  BlogPost.find(query)
+  BlogPost.find({ userid: { $exists: true, $ne: null } })
     .populate('userid')
     .sort({ datePosted: -1 })
     .then((blogPosts) => {
